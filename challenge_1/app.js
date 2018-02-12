b58ed53f203003ab0dc0bb674ba64db8742d9368
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-02-12 12:26:49 -0800

    Made board, working on checkSpot function

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,44 @@
+/* 
+1. Need to create user interface w/o css
+  - Use hr to make rows
+  - Need to figure out how to make columns
+2. Make gameboard in js by creating an array of arrays
+  - 
+*/
+
+var gameboard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+]
+
+var playerX= {
+  piece: 'X',
+  turn: true,
+}
+
+var playerO = {
+  piece: 'O',
+  turn: false,
+}
+
+var checkSpot = function() {
+  for (var i = 0; i < gameboard.length; i++) {
+    var row = gameboard[i]
+    for (var j = 0; j < row.length; j++) {
+      if (row[j] === null && playerX.turn) {
+        row[j] = playerX.piece;
+        playerX.turn = false;
+      } else if (row[j] === null && playerO.turn) {
+        row[j] = playerO.piece;
+        playerO.turn = false;
+      }
+    }
+  }
+}
+
+checkSpot();
+
+
 
 
