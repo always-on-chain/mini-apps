46bae944979dd3a0d18a70fc1fd6a77de6022eef
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-02-13 13:54:50 -0800

    got post requests to work

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,55 @@
 
+//Model
+var app = {
+  server: 'http://localhost:3000',
+  send: function(data) {
+    $.ajax({
+      url: app.server,
+      type: 'POST',
+      data: data,
+      success: function(data) {
+        console.log('SUCCESS!', data)
+      },
+      error: function(error) {
+        console.log('error', error);
+      }
+    })
+  },
+  fetch: function() {
+    $.ajax({
+      url: app.server,
+      type: 'GET',
+      data: data,
+      success: function(data) {
+        console.log(data);
+      },
+      error: function(error) {
+        console.error('Failed to fetch messages', error);
+      }
+    })
+  },
+  sendData: function() {
+    var data = $('#data').val();
+    console.log(data);
+    app.send(data);
+  },
 
+} 
+
+//Control
+// var sendData = function() {
+//   var data = $('#data').val();
+//   console.log('hi')
+//   app.send(data);
+// }
+
+
+$('#submit').on('click', function() {
+  app.sendData();
+});
+
+//View
+// var renderData = function(data) {
+
+
+// }
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,24 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
+app.get('/', function(req, res) {
+  console.log(req)
+  res.send('req');
+})
+
+app.post('/', function(req, res) {
+  // res.setHeader('Content-Type', 'application/json');
+  res.send(req.body);
+})
+
+// app.set('port', 3000);
+
+app.listen(3000, function() {
+  console.log('listening');
+});
